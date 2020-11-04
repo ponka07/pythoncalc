6d389fa7f3ff04c042655e62b76a55a4e1784608
yskvl <johntindall9@gmail.com>
2020-11-04 13:39:13 -0600

    shortened

--- calculator.py
+++ calculator.py
@@ -1,16 +1,4 @@
 print("this will only work with 2 numbers")
-def plus(x, y):
-    return x + y
-
-def minus(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    return x / y
-
 print('what do you want to calculate?')
 print("1: add")
 print("2: subtract")
@@ -24,16 +12,16 @@ while True:
         num2 = float(input('second number: '))
         
         if choice == '1':
-            print(num1, 'plus', num2, 'equals', plus(num1, num2))
+            print(num1, 'plus', num2, 'equals', num1 + num2)
         
         elif choice == '2':
-            print(num1, 'minus', num2, 'equals', minus(num1, num2))
+            print(num1, 'minus', num2, 'equals', num1 - num2)
         
         elif choice == '3':
-            print(num1, 'divided by', num2, 'equals', multiply(num1, num2))
+            print(num1, 'multiplied by', num2, 'equals', num1 * num2)
         
         elif choice == '4':
-            print(num1, 'divided by', num2, 'equals', divide(num1, num2))
+            print(num1, 'divided by', num2, 'equals', num1 / num2)
         break
     else:
         print('enter a proper number')
